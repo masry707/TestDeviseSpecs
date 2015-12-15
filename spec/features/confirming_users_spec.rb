82d0633c648cb506507d3decd4bace79c7418838
Ahmed El Masry <ahmed.elmasry707@gmail.com>
2015-12-15 13:10:31 +0200

    confirming email spec

--- spec/features/confirming_users_spec.rb
+++ spec/features/confirming_users_spec.rb
@@ -3,13 +3,22 @@ require 'rails_helper'
 RSpec.feature 'confirming users' do
 
   before do
-    @unconfirmed_user = create :user, :unconfirmed_user
+    @unconfirmed_user = build :user, :unconfirmed_user
   end
 
   scenario do
-    visit "/users/confirmation?confirmation_token=#{@unconfirmed_user.confirmation_token}"
+    visit '/'
+    click_link 'Sign up'
+
+    fill_in 'user_name', with: @unconfirmed_user.name
+    fill_in 'user_email', with: @unconfirmed_user.email
+    fill_in 'user_password', with: @unconfirmed_user.password
+    fill_in 'user_password_confirmation', with: @unconfirmed_user.password
+    click_button 'Sign up'
+
+    open_email(@unconfirmed_user.email)
+    current_email.click_link('Confirm my account')
 
     expect(page).to have_content('Your email address has been successfully confirmed.')
-    expect(page.current_path).to eq(new_user_session_path)
   end
 end
